934ab3abc4b43bea15074df3608a7c66f6b35535
MacroUniverse <270174408@qq.com>
2023-01-13 07:10:21 -0600

    Create pull_all.sh

---
+++ pull_all.sh
@@ -0,0 +1,10 @@
+cd littleshi.cn; git pull wuli master; git push; cd ../;
+cd user-notes/; git pull wuli master; git push; cd ../;
+cd PhysWiki; git pull wuli master; git push; cd ../;
+cd PhysWiki-backup; git pull wuli master; git push; cd ../;
+cd PhysWikiScan; git pull; cd ../; git push;
+
+cd Notes; git pull; cd ../;
+cd Private; git pull; cd ../;
+cd SLISC0; git pull; cd ../;
+cd FEDVR-TDSE; git pull; cd ../;
