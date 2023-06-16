bd552342d702568cfb0f86d3523b74cec2260c4f
MacroUniverse <270174408@qq.com>
2023-06-16 01:13:28 -0500

    amend

--- chwuli.sh
+++ chwuli.sh
@@ -3,6 +3,6 @@ group=$(stat -c '%G' .)
 echo group is $group
 chmod 2750 .
 chgrp $group -R .
-find . -type d -exec chmod 775 {} \;
+find . -mindepth 1 -type d -exec chmod 775 {} \;
 find . -type f ! -executable -exec chmod 664 {} \;
 find . -type f -executable -exec chmod 775 {} \;
