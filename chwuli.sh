b449404473a1ac27f84b73ad2bca2027ba24b3ec
MacroUniverse <270174408@qq.com>
2023-06-16 17:05:57 -0500

    Update chwuli.sh

--- chwuli.sh
+++ chwuli.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 group=$(stat -c '%G' .)
 echo group: $group
-chmod 2750 .
+chmod 2770 .
 chgrp $group -R .
 find . -mindepth 1 -type d -exec chmod 775 {} \;
 find . -type f ! -executable -exec chmod 664 {} \;
