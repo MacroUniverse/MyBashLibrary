83de0ae24c64c6e795e1777cf918085601d39f4e
MacroUniverse <270174408@qq.com>
2024-05-19 18:03:36 -0500

    amend

--- db_restore.sh
+++ db_restore.sh
@@ -6,16 +6,18 @@ file_in=$1;
 num_args=$#
 
 if [ $num_args -eq 1 ]; then
-    if [[ $file_in == *.sql ]]; then
+	if [[ $file_in == *.db.sql ]]; then
+		file_out="${file_in%.sql}"
+	elif  [[ $file_in == *.sql ]]; then
 		file_out="${file_in%.sql}.db"
 	else
 		file_out="$file_in.db"
 	fi
 elif [ $num_args -eq 2 ]; then
-        file_out=$2
+		file_out=$2
 else
-        echo "ERROR: usage: cmd <file_in> [<file_out>]"
-        exit 1
+		echo "ERROR: usage: cmd <file_in> [<file_out>]"
+		exit 1
 fi
 
 sqlite3 $file_out < $file_in
