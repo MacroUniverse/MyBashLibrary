4f0062e34c269a8944fb48633a2a8b67a143d5d4
MacroUniverse <270174408@qq.com>
2024-05-19 17:57:31 -0500

    Create db_restore.sh

---
+++ db_restore.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# restore a sqlite db file from sql file
+# cmd <file_in> [<file_out>]
+
+file_in=$1;
+num_args=$#
+
+if [ $num_args -eq 1 ]; then
+    if [[ $file_in == *.sql ]]; then
+		file_out="${file_in%.sql}.db"
+	else
+		file_out="$file_in.db"
+	fi
+elif [ $num_args -eq 2 ]; then
+        file_out=$2
+else
+        echo "ERROR: usage: cmd <file_in> [<file_out>]"
+        exit 1
+fi
+
+sqlite3 $file_out < $file_in
