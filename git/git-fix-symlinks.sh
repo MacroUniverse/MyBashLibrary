2033b52503139324e38a3442a35502ad885d584c
MacroUniverse <270174408@qq.com>
2024-05-23 00:02:24 -0500

    Update git-fix-symlinks.sh
    now supports WSL2

--- git/git-fix-symlinks.sh
+++ git/git-fix-symlinks.sh
@@ -1,19 +1,32 @@
 # must be called at the same directory of `.git` folder
 # loop through all symlinks in repo
+
+NTFSprefix=IntxLNK$'\001'
+
 for file in \
 	$(git ls-files -s | egrep '^120000' | awk '$NF != "" {print $NF}');
 do
 	if [[ ! -L "$file" ]]; then
 		# current file is not a symlink and not empty
-		printf "[fixing] $file -> $(cat $file)\n"
+		# Linux might add a suffix "IntxLNK" for a symlink on NTFS
+		dest=$(cat $file | tr -d '\0')
+		dest="${dest#$NTFSprefix}"
+
+		printf "[fixing] $file -> $dest\n"
 		if [[ -s "$file" ]]; then
 			# file is not empty
-			ln -sf "$(cat $file)" "$file"
+			ln -sf "$dest" "$file"
 			if [[ ! -e "$file" ]]; then # dest exist
 				echo "warning: destination doesn't exist."
+				git checkout $file
 			fi
 		else
-			echo "file is empty!"
+			echo "error: file is empty!"
+		fi
+	else
+		if [[ ! -e "$file" ]]; then # dest exist
+			echo "warning: destination doesn't exist."
+			git checkout $file
 		fi
 	fi
 done
