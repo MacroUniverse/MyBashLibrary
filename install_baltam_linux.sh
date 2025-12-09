0cb243cf64359695a25c2f846ce55f81fa446a66
MacroUniverse <270174408@qq.com>
2025-12-09 18:34:49 +0800

    1

--- install_baltam_linux.sh
+++ install_baltam_linux.sh
@@ -1,14 +1,22 @@
-# windows 安装北太天元并自动记录版本和备份关键文件夹
+# Linux 安装北太天元并自动记录版本和备份关键文件夹
 # 设置
 set -e
+BT_ROOT="/opt/Baltamatica"
+deb_file=$(ls baltamatica_*.deb)
+license_file="/mnt/c/baltamatica/licenses/LICENSE"
 
-BT_ROOT="/c/baltamatica"
-exe_file=(baltamatica_*.exe)
-license_file="/c/baltamatica_*/licenses/LICENSE"
+echo "=== 安装，需要 cd 到 deb 所在目录 ==="
+if [ -d "$BT_ROOT" ]; then
+    echo "$BT_ROOT 已存在！"
+	exit
+fi
+sudo apt update
+sudo apt purge baltamatica -y
+sudo apt install "./$deb_file" -y
 
 echo "=== 记录版本号 ==="
 cd "$BT_ROOT"
-touch "$(basename "$exe_file" .exe)"
+touch "$BT_ROOT/$(basename "$deb_file" .deb)"
 
 echo "=== 备份要更新的文件夹 ==="
 cp -r lib lib-original
@@ -19,7 +27,7 @@ cp -r external/python/{python3.12.7,python3.12.7-original}
 
 echo "=== 隐藏工具箱（不需要隐藏插件，不会默认加载） ==="
 mkdir toolbox/hide
-mv toolbox/{*,hide} &> /dev/null
+mv toolbox/{*,hide} > /dev/null 2>&1
 mv toolbox/hide/CodeGeneration toolbox
 
 echo "=== LICENSE ==="
--- install_baltam_mac.sh
+++ install_baltam_mac.sh
@@ -1,24 +1,24 @@
+# mac 安装北太天元并自动记录版本和备份关键文件夹
 # 设置
 set -e
-BT_ROOT="/opt/Baltamatica"
-deb_file=$(ls baltamatica_*.deb)
-license_file="/mnt/c/baltamatica/licenses/LICENSE"
+BT_ROOT="/Applications/Baltamatica.app/Contents"
+zip_file=$(ls baltamatica_*.zip)
+license_file="LICENSE"
 
-echo "=== 安装，需要 cd 到 deb 所在目录 ==="
+echo "=== 安装，需要 cd 到 zip 所在目录 ==="
 if [ -d "$BT_ROOT" ]; then
     echo "$BT_ROOT 已存在！"
-	exit
+    exit
 fi
-sudo apt update
-sudo apt purge baltamatica -y
-sudo apt install "./$deb_file" -y
+unzip -q "./$zip_file"
+sudo mv Baltamatica.app /Applications/
 
 echo "=== 记录版本号 ==="
 cd "$BT_ROOT"
-touch "$BT_ROOT/$(basename "$deb_file" .deb)"
+touch "$BT_ROOT/$(basename "$zip_file" .zip)"
 
 echo "=== 备份要更新的文件夹 ==="
-cp -r lib lib-original
+cp -r Frameworks Frameworks-original
 cp -r plugins/{Python,Python-original}
 cp -r plugins/{SymPy,SymPy-original}
 cp -r toolbox/{CodeGeneration,CodeGeneration-original}
@@ -31,7 +31,7 @@ mv toolbox/hide/CodeGeneration toolbox
 
 echo "=== LICENSE ==="
 if [ -f "$license_file" ]; then
-	echo "警告：[$license_file] 不存在，请手动复制"
-	exit
+    echo "警告：[$license_file] 不存在，请手动复制"
+    exit
 fi
 cp "$license_file" licenses/
